4a0ad4543ed2db9f3df926723a1dddbb493119dc
Tryzack <joseandres102003@gmail.com>
2024-03-29 03:11:09 -0400

    update

--- sockets.js
+++ sockets.js
@@ -1,44 +1,43 @@
 module.exports = (io) => {
-	let line_history = {};
+	let rooms = {};
 
 	io.on("connection", (socket) => {
-		/* 		socket.on("draw_line", (data) => {
-			line_history.push({ line: data.line, color: data.color, width: data.size });
-			io.emit("draw_line", { line: data.line, color: data.color, width: data.size });
-		});
-
-		socket.on("clear_canvas", () => {
-			line_history = [];
-			io.emit("clear_canvas");
-		}); */
-
 		socket.on("join-room", (roomId, userId, customName) => {
-			for (let i in line_history[roomId]) {
-				socket.emit("draw_line", {
-					line: line_history[roomId][i].line,
-					color: line_history[roomId][i].color,
-					width: line_history[roomId][i].width,
-				});
+			if (!rooms[roomId]) {
+				rooms[roomId] = { users: 1 };
+			} else {
+				rooms[roomId].users++;
+				for (let i in rooms[roomId].line_history) {
+					socket.emit("draw_line", {
+						line: rooms[roomId].line_history[i].line,
+						color: rooms[roomId].line_history[i].color,
+						width: rooms[roomId].line_history[i].width,
+					});
+				}
 			}
 
 			socket.join(roomId);
 			socket.to(roomId).emit("user-connected", userId, customName);
 
 			socket.on("draw_line", (data) => {
-				if (!line_history[roomId]) {
-					line_history[roomId] = [];
+				if (!rooms[roomId].line_history) {
+					rooms[roomId].line_history = [];
 				}
-				line_history[roomId].push({ line: data.line, color: data.color, width: data.size });
+				rooms[roomId].line_history.push({ line: data.line, color: data.color, width: data.size });
 				io.to(roomId).emit("draw_line", { line: data.line, color: data.color, width: data.size });
 			});
 
 			socket.on("clear_canvas", () => {
-				line_history[roomId] = [];
+				rooms[roomId].line_history = [];
 				io.to(roomId).emit("clear_canvas");
 			});
 
 			socket.on("disconnect", () => {
 				socket.to(roomId).emit("user-disconnected", userId);
+				rooms[roomId].users--;
+				if (rooms[roomId].users === 0) {
+					delete rooms[roomId];
+				}
 			});
 		});
 	});
